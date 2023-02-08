f664115cf05f34735feba716fe4ddedfd5b167a4
Paolo Di Bucci <paolod@wa.venueslive.com.au>
2023-02-08 16:42:24 +0800

    Small change to program.cs. No actions should run.

--- aspnetserver/Program.cs
+++ aspnetserver/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
             builder
             .AllowAnyMethod()
             .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+            .WithOrigins("http://localhost:3000", "https://happy-dune-0e6349800.2.azurestaticapps.net");
         });
 });
 
